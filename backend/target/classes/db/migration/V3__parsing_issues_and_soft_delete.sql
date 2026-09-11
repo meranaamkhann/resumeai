3e28e0a1f40803c25a606207255802be1b5d1fb6
Asad Khan <khanasad1907@gmail.com>
2026-09-11 15:47:25 +0530

    Ignore Maven build output

--- backend/target/classes/db/migration/V3__parsing_issues_and_soft_delete.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE resume_documents ADD COLUMN parsing_issues TEXT;
-ALTER TABLE resume_documents ADD COLUMN deleted_at TIMESTAMPTZ;
-CREATE INDEX idx_resumes_deleted_at ON resumes(deleted_at);
